71be8b6beaeab9917b0bcac7cc4d76f6adc1c29b
zakhar bozhenko <zakhar.bozhenko@cyfroweszkoly.pl>
2024-02-15 16:06:20 +0100

    next upload

--- app.js
+++ app.js
@@ -36,11 +36,33 @@ app.get("/api/ponczki", async (req, res) => {
 app.get("/api/ponczki/:id", async (req, res) => {
     try {
         const currentID = +req.params.id;
-        if (isNaN(currentID)) return res.status(400).json({message: "Invalid ID"});
+        if (isNaN(currentID))
+            return res.status(400).json({message: "Invalid ID"});
+
         const collection = db.collection("handmode");
         const ponczekDokument = await collection.findOne({id: currentID});
-        if (!ponczekDokument) return res.status(404).json({message: "Dokument not found"});
+
+        if (!ponczekDokument)
+            return res.status(404).json({message: "Dokument not found"});
+        
         res.send(ponczekDokument);
+    } catch (err) {
+        res.status(500).json({error: err.message});
+    }
+});
+
+app.delete("/api/ponczki/:id", async (req, res) => {
+    try {
+        const myID = +req.params.id;
+        if (isNaN(myID))
+            res.status(400).json({message: "Invalid ID"});
+    
+        const collection = db.collection("handmode");
+        const result = await collection.deleteOne({id: myID});
+        if (result.deletedCount === 0)
+            return res.status(404).json({message: "Docunemt Not Found"});
+    
+        res.json({message: "Your document has deleted succesfully already"});
     } catch (err) {
         res.status(500).json({error: err.message});
     }
@@ -48,14 +70,65 @@ app.get("/api/ponczki/:id", async (req, res) => {
 
 app.post("/api/ponczki", async (req, res) => {
     try {
-        const collection = db.collection("handmode");
         const newPonczekDokument = req.body;
-        console.log(newPonczekDokument.name);
+        console.log(newPonczekDokument)
+        res.send({a:32})
+        // if (!isValidDocument(newPonczekDokument)){
+        //     return res.status(400).json({message: "Invalid document format"});
+        // }
+        // console.log(newPonczekDokument);
+        // const collection = db.collection("handmode");
+        // const result = await collection.insertOne(newPonczekDokument);
 
-        if (!newPonczekDokument || typeof newPonczekDokument !== Object) return res.status(400).json({message: "Invalid document format"});
-        const result = await collection.insertOne(newPonczekDokument);
-        if (!result.acknowledged) return res.status(500).json({message: "Failed to add the document"});
-        res.status(201).json({message: "Document added successfully", insertID: result.insertedId});
+        // if (!result.acknowledged) // if true — insert accept
+        //     return res.status(500).json({message: "Failed to add the document"});
+        // res.status(201).json({message: "Document added successfully", insertID: result.insertedId});
+    } catch (err) {
+        res.status(500).json({error: err.message});
+    }
+});
+
+app.put("/api/ponczki/:id", async (req, res) => {
+    try {
+        const myID = parseInt(req.params.id);
+        if (isNaN(myID))
+            return res.status(400).json({message: "Invalid ID"});
+
+        const collection = db.collection("handmode");
+        const docToUpdate = req.body;
+        if (!isValidDocument(docToUpdate))
+            return res.status(400).json({message: "Invalid document format"});
+
+        const result = await collection.replaceOne({id: myID}, docToUpdate);
+        if (result.matchedCount === 0)
+            return res.status(404).json({message: "Document Not Found"});
+        if (result.modifiedCount === 0)
+            return res.status(400).json({message: "Nie chciało mie się nic zmenić"});
+
+        res.json({message: "Zamieniłem dokumenty"});
+    } catch (err) {
+        res.status(500).json({error: err.message});
+    }
+});
+
+app.patch("/api/ponczki/:id", async (req, res) => {
+    try {
+        const myID = parseInt(req.params.id);
+        if (isNaN(myID))
+            return res.status(400).json({message: "Invalid ID"});
+
+        const collection = db.collection("handmode");
+        const docToUpdate = req.body;
+        if (!isValidDocument(docToUpdate))
+            return res.status(400).json({message: "Invalid document format"});
+
+        const result = await collection.replaceOne({id: myID}, {$set: docToUpdate});
+        if (result.matchedCount === 0)
+            return res.status(404).json({message: "Document Not Found"});
+        if (result.modifiedCount === 0)
+            return res.status(400).json({message: "Nie chciało mie się nic zmenić"});
+
+        res.json({message: "Zamieniłem dokumenty"});
     } catch (err) {
         res.status(500).json({error: err.message});
     }
@@ -69,3 +142,9 @@ process.on("SIGINT", () => {
         process.exit();
     })
 });
+
+
+
+function isValidDocument(doc) {
+    return doc && typeof(doc) === "object" && Object.keys(doc).length > 0;
+}
